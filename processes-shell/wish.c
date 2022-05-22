5a7462ff2fa706f7d22e88b9907cac960de58157
Cameron Dignan <CamBDignan@gmail.com>
2022-05-22 16:02:42 -0700

    exit built-in

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -15,23 +15,31 @@ int main(int argc, char* argv[])
     lineSize = getline(&line, &len, stdin);
     line[strcspn(line, "\n")] = 0; // remove newline char
 
+    char* line2 = strdup(line);
+    int count = 0;
+
+    // get number of strings passed in by user
+    while (strsep(&line, " ") != NULL )
+      ++count;
+
+    char* myArgs[count + 1];
+    int index = 0;
+
+    // build arguments array
+    while ((*(myArgs + index) = strsep(&line2, " ")) != NULL)
+      ++index;
+
+    myArgs[count] = NULL;
+
+    // check for built-in commands
+    if (strcmp(myArgs[0], "exit") == 0)
+      exit(0);
+
+    // if not a built-in, call execv on child process
     int rc = fork();
 
     if (rc == 0) // child process
     {
-      char* line2 = strdup(line);
-      int count = 0;
-
-      while (strsep(&line, " ") != NULL )
-        ++count;
-
-      char* myArgs[count + 1];
-      int index = 0;
-
-      while ((*(myArgs + index) = strsep(&line2, " ")) != NULL)
-        ++index;
-
-      myArgs[count] = NULL;
       execv(myArgs[0], myArgs);
     }
     else // parent process
